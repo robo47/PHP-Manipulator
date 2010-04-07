0f49320ffd1ccf7b2707fe4b7458759ba186751d
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-08 00:44:44 +0200

    Added ezc-Stuff to autoloader

--- library/PHP/Manipulator/Autoloader.php
+++ library/PHP/Manipulator/Autoloader.php
@@ -6,13 +6,39 @@ class Autoloader
 {
 
     /**
+     * Loads the ezc-Autoloader-Class
+     */
+    public function __construct()
+    {
+        require_once 'ezc/Base/base.php';
+    }
+    /**
      * @param string $classname
      */
     public function autoload($classname)
     {
+        if ($this->_isEzcClass($classname)) {
+
+            \ezcBase::autoload($classname);
+            return true;
+        }
+
         $filename = str_replace('\\', DIRECTORY_SEPARATOR, $classname);
         $filename = str_replace('_', DIRECTORY_SEPARATOR, $filename);
+        if ($filename[0] === \DIRECTORY_SEPARATOR) {
+            $filename = \substr($filename, 1);
+        }
         include $filename . '.php';
+    }
+
+    /**
+     *
+     * @param string $classname
+     * @return boolean
+     */
+    protected function _isEzcClass($classname)
+    {
+        return false !== \strpos($classname, 'ezc') || false !== \strpos($classname, '\ezc');
     }
 
     /**
