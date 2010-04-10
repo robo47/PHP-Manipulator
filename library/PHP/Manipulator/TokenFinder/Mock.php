df6ba15d143aa2f9febf716abeb50e5c20fba9e5
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-10 16:05:56 +0200

    TokenFinder_Mock + unittests

---
+++ library/PHP/Manipulator/TokenFinder/Mock.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenFinder;
+use PHP\Manipulator\TokenContainer;
+use PHP\Manipulator\Token;
+
+class Mock
+extends TokenFinder
+{
+    /**
+     * @var \PHP\Manipulator\TokenFinder\Result
+     */
+    public $result = null;
+
+    /**
+     *
+     * @param \PHP\Manipulator\TokenFinder\Result $result
+     */
+    public function __construct(Result $result = null)
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * Finds tokens
+     *
+     * @param \PHP\Manipulator\Token $token
+     * @param \PHP\Manipulator\TokenContainer $container
+     * @param mixed $params
+     * @return \PHP\Manipulator\TokenFinder\Result
+     */
+    public function find(Token $token, TokenContainer $container, $params = null)
+    {
+        return $this->result;
+    }
+}
---
+++ tests/Tests/PHP/Manipulator/TokenFinder/MockTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenFinder\Mock;
+use PHP\Manipulator\Token;
+
+/**
+ * @group TokenFinder_Mock
+ */
+class MockTest
+extends \Tests\TestCase
+{
+    /**
+     * @covers \PHP\Manipulator\TokenFinder\Mock::__construct
+     */
+    public function testConstruct()
+    {
+        $result = new Result();
+        $mock = new Mock($result);
+        $this->assertSame($result, $mock->result);
+    }
+
+    /**
+     * @return array
+     */
+    public function findProvider()
+    {
+        $data = array();
+        $path = '/TokenFinder/Mock/';
+
+        #0
+        $data[] = array(
+            new Token('Foo'),
+            null,
+            $this->getContainerFromFixture($path . 'input0'),
+            new Result()
+        );
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider findProvider
+     * @covers \PHP\Manipulator\TokenFinder\Mock::find
+     * @covers \PHP\Manipulator\TokenFinder\Mock::<protected>
+     */
+    public function testFind($token, $params, $container, $expectedResult)
+    {
+        $finder = new Mock($expectedResult);
+        $actualResult = $finder->find($token, $container, $params);
+        $this->assertFinderResultsMatch($expectedResult, $actualResult);
+    }
+}
