d721dc011cc0fbb4c21043fd124287c789e3051a
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-11 00:12:15 +0200

    TokenFinder\FunctionFinder + unittests

---
+++ library/PHP/Manipulator/TokenFinder/FunctionFinder.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenFinder;
+use PHP\Manipulator\TokenContainer;
+use PHP\Manipulator\Token;
+
+class FunctionFinder
+extends TokenFinder
+{
+    /**
+     * Finds tokens
+     *
+     * @param \PHP\Manipulator\Token $token
+     * @param \PHP\Manipulator\TokenContainer $container
+     * @param mixed $params
+     * @return \PHP\Manipulator\TokenFinder\Result
+     */
+    public function find(Token $token, TokenContainer $container, $params = null)
+    {
+        if (!$this->evaluateConstraint('IsType', $token, T_FUNCTION)) {
+            throw new \Exception('Start-token is not T_FUNCTION: ' . \token_name($token->getType()));
+        }
+
+        $pos = $container->getOffsetByToken($token);
+        $iterator = $container->getIterator();
+        $iterator->seek($pos);
+
+        if($this->_includeMethodProperties($params) && !$this->_includePhpDoc($params)) {
+            // travel reverse as long as there is only whitespace and stuff
+            $iterator->previous();
+            while($iterator->valid()) {
+                if (!$this->evaluateConstraint('IsType', $iterator->current(), array(T_WHITESPACE, T_PUBLIC, T_COMMENT, T_DOC_COMMENT, T_PUBLIC, T_PROTECTED, T_PRIVATE, T_STATIC))) {
+                    $iterator->next();
+                    while($iterator->valid()) {
+                        if (!$this->evaluateConstraint('IsType', $iterator->current(), array(T_PUBLIC,T_PROTECTED, T_PRIVATE, T_STATIC))) {
+                            $iterator->next();
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                }
+                $iterator->previous();
+            }
+        }
+
+        if($this->_includePhpDoc($params)) {
+            // travel reverse as long as there is only whitespace and stuff
+            $iterator->previous();
+            while($iterator->valid()) {
+                if (!$this->evaluateConstraint('IsType', $iterator->current(), array(T_WHITESPACE, T_PUBLIC, T_COMMENT, T_DOC_COMMENT, T_PUBLIC, T_PROTECTED, T_PRIVATE, T_STATIC))) {
+                    $iterator->next();
+                    while($iterator->valid()) {
+                        if (!$this->evaluateConstraint('IsType', $iterator->current(), array(T_DOC_COMMENT, T_PUBLIC,T_PROTECTED, T_PRIVATE, T_STATIC))) {
+                            $iterator->next();
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                }
+                $iterator->previous();
+            }
+        }
+
+
+
+        $result = new Result();
+        $inside = false;
+        $level = 0;
+        while ($iterator->valid()) {
+            $token = $iterator->current();
+            $result->addToken($token);
+
+            if ($this->evaluateConstraint('IsOpeningCurlyBrace', $token)) {
+                $inside = true;
+                $level++;
+            }
+
+            if ($this->evaluateConstraint('IsClosingCurlyBrace', $token)) {
+                $level--;
+            }
+
+            // abstract methods or interface-methods
+            if (false === $inside && $token->getValue() == ';') {
+                break;
+            }
+
+            // last curly-brace closed
+            if (true === $inside && $level == 0) {
+                break;
+            }
+
+            $iterator->next();
+        }
+        return $result;
+    }
+
+    protected function _includePhpDoc($params)
+    {
+        if(is_array($params) && isset($params['includePhpdoc'])) {
+            return (bool)$params['includePhpdoc'];
+        } else {
+            return false;
+        }
+    }
+
+    // wheter to check for public/protected/private
+    protected function _includeMethodProperties($params)
+    {
+        if(is_array($params) && isset($params['includeMethodProperties'])) {
+            return (bool)$params['includeMethodProperties'];
+        } else {
+            return false;
+        }
+    }
+}
---
+++ tests/Tests/PHP/Manipulator/TokenFinder/FunctionFinderTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenFinder\FunctionFinder;
+use PHP\Manipulator\TokenContainer;
+use PHP\Manipulator\Token;
+
+/**
+ * @group TokenFinder_FunctionFinder
+ */
+class FunctionFinderTest
+extends \Tests\TestCase
+{
+
+    /**
+     * @return array
+     */
+    public function findProvider()
+    {
+        $data = array();
+        $path = '/TokenFinder/FunctionFinder/';
+
+        #0 function
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input0'),
+            $c[3],
+            array(),
+            $this->getResultFromContainer($c, 3, 14),
+        );
+
+        #1 Class method
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input1'),
+            $c[11],
+            array(),
+            $this->getResultFromContainer($c, 11, 22),
+        );
+
+        #2 function with code and braces in it
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input2'),
+            $c[3],
+            array(),
+            $this->getResultFromContainer($c, 3, 43),
+        );
+
+        #3 class method with code and braces in it
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input3'),
+            $c[13],
+            array(),
+            $this->getResultFromContainer($c, 13, 53),
+        );
+
+        #4 abstract class method
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input4'),
+            $c[15],
+            array(),
+            $this->getResultFromContainer($c, 15, 23),
+        );
+
+        #5 including prefixes
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input5'),
+            $c[15],
+            array('includeMethodProperties' => true),
+            $this->getResultFromContainer($c, 11, 55),
+        );
+
+        #6 including phpdoc
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input6'),
+            $c[15],
+            array('includePhpdoc' => true),
+            $this->getResultFromContainer($c, 9, 55),
+        );
+
+        #6 including phpdoc and methodproperties
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input7'),
+            $c[15],
+            array('includePhpdoc' => true, 'includeMethodProperties' => true),
+            $this->getResultFromContainer($c, 9, 55),
+        );
+
+        #8 including phpdoc with some ugly comments inbetween
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input8'),
+            $c[21],
+            array('includePhpdoc' => true),
+            $this->getResultFromContainer($c, 9, 61),
+        );
+
+        // abstract functions / methods
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider findProvider
+     * @covers \PHP\Manipulator\TokenFinder\FunctionFinder::find
+     * @covers \PHP\Manipulator\TokenFinder\FunctionFinder::<protected>
+     */
+    public function testFind($container, $token, $params, $expectedResult)
+    {
+        $finder = new FunctionFinder();
+        $actualResult = $finder->find($token, $container, $params);
+        $this->assertFinderResultsMatch($expectedResult, $actualResult);
+    }
+}
