74badd449a82b89c618142cb94edc7b0e73a090f
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-13 15:53:10 +0200

    Refactored \Rule\RemoveComments

--- library/PHP/Manipulator/Rule/RemoveComments.php
+++ library/PHP/Manipulator/Rule/RemoveComments.php
@@ -6,11 +6,10 @@ use PHP\Manipulator\Rule;
 use PHP\Manipulator\TokenContainer;
 use PHP\Manipulator\Token;
 
-// @todo single-line-comments containing a line-break should be replaced with a whitespace-newline
 class RemoveComments
 extends Rule
 {
-
+    
     public function init()
     {
         if (!$this->hasOption('removeDocComments')) {
@@ -29,76 +28,33 @@ extends Rule
     public function apply(TokenContainer $container)
     {
         $iterator = $container->getIterator();
-
         while ($iterator->valid()) {
-            $wasSingleLine = false;
             $token = $iterator->current();
-            /* @var $token PHP\Manipulator\Token */
             if ($this->_isCommentAndShouldBeRemoved($token)) {
-                // replace comment with whitespace
-                if(0 === strpos($token->getValue(), '//')) {
-                    $wasSingleLine = true;
-                }
-                if ($this->_normalOneLineComment($token, $container)) {
+                if ($this->_isOneLineComment($token, $container)) {
                     $token->setType(T_WHITESPACE);
+                    // @todo checking code which linebreak is used and use that
                     $token->setValue("\n");
-                } elseif ($this->_multiNotFollowedByWhitespaceComment($token, $container)) {
-                    $container->removeToken($token);
-                } elseif ($this->_multiFollowedByWhitespaceWithBreakComment($token, $container)) {
-                    $container->removeToken($token);
                 } else {
                     $container->removeToken($token);
                 }
-
             }
             $iterator->next();
         }
-
         $container->retokenize();
     }
 
-
-    protected function _normalOneLineComment(Token $token, TokenContainer $container)
+    /**
+     * @param Token $token
+     * @param TokenContainer $container
+     * @return boolean
+     */
+    protected function _isOneLineComment(Token $token, TokenContainer $container)
     {
         if (true == preg_match('~^(\/\/|#){1,}~', $token->getValue())) {
             return true;
         }
         return false;
-    }
-
-
-    protected function _multiNotFollowedByWhitespaceComment(Token $token, TokenContainer $container)
-    {
-        if (false !== preg_match('~^(\/\*){1,}~', $token->getValue())) {
-            // nächster Token ist KEIN WHITESPACE
-            if (!$this->_followedByWhitespace($token, $container)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected function _multiFollowedByWhitespaceWithBreakComment(Token $token, TokenContainer $container)
-    {
-        if (false !== preg_match('~^(\/\*){1,}~', $token->getValue())) {
-            // nächster Token ist WHITESPACE
-            if ($this->_followedByWhiteContainingLinebreak($token, $container)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected function _followedByWhiteContainingLinebreak(Token $token, TokenContainer $container)
-    {
-        $nextToken = $container->getNextToken($token);
-        return ($nextToken->getType() === T_WHITESPACE) && (preg_match('~(\r|\n)~', $nextToken->getValue()));
-    }
-
-    protected function _followedByWhitespace(Token $token, TokenContainer $container)
-    {
-        $nextToken = $container->getNextToken($token);
-        return ($nextToken->getType() === T_WHITESPACE);
     }
 
     /**
