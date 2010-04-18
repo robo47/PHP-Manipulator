d68fa41b3b4887431fbe8960529ef3699a6e2e60
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-18 11:58:20 +0200

    TestHelper namespaced

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,4 +1,8 @@
 <?php
+namespace Tests;
+
+use PHP\Manipulator\Autoloader;
+
 error_reporting(E_ALL | E_STRICT);
 
 define('BASE_PATH', realpath(dirname(__FILE__ ) . '/../'));
@@ -14,4 +18,4 @@ set_include_path(implode($paths, PATH_SEPARATOR));
 
 // Register autoloader
 require_once 'PHP/Manipulator/Autoloader.php';
-\PHP\Manipulator\Autoloader::register();
+Autoloader::register();
