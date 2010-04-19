64e0444567d59a064a654d311b90708662f91897
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-19 22:23:04 +0200

    Empty TestCaseTest (incomplete tests) and added to phpunitTests.xml

---
+++ tests/Tests/TestCaseTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests;
+
+use Tests\Util;
+use PHP\Manipulator\Token;
+use PHP\Manipulator\TokenContainer;
+
+/**
+ * @group TestCase
+ */
+class TestCaseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Tests\Testcase::getFixtureFileContent
+     */
+    public function testGetFixtureFileContent()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::getResultFromContainer
+     */
+    public function testGetResultFromContainer()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::getContainerFromFixture
+     */
+    public function testGetContainerFromFixture()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::assertCount
+     */
+    public function testAssertCount()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::assertTokenMatch
+     */
+    public function testAssertTokenMatch()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::assertTokenContainerMatch
+     */
+    public function testAssertTokenContainerMatch()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::checkAsptags
+     */
+    public function testCheckAsptags()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::checkShorttags
+     */
+    public function testCheckShorttags()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::getConfig
+     */
+    public function testGetConfig()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * @covers Tests\Testcase::getXmlConfig
+     */
+    public function testGetXmlConfig()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+}
