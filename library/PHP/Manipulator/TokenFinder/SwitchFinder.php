453dba12e0c8c51bf0a1247b4064f44566c39372
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-20 11:53:43 +0200

    \TokenFinder\SwitchFinder + test

---
+++ library/PHP/Manipulator/TokenFinder/SwitchFinder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder;
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenContainer;
+use PHP\Manipulator\Token;
+
+class SwitchFinder
+extends TokenFinder
+{
+
+    /**
+     * Finds tokens
+     *
+     * @param \PHP\Manipulator\Token $token
+     * @param \PHP\Manipulator\TokenContainer $container
+     * @param mixed $params
+     * @return \PHP\Manipulator\TokenFinder\Result
+     */
+    public function find(Token $token, TokenContainer $container, $params = null)
+    {
+        if (!$this->evaluateConstraint('IsType', $token, T_SWITCH)) {
+            throw new Exception('Starttoken is not T_SWITCH');
+        }
+        $result = new Result();
+        $iterator = $container->getIterator();
+        $key = $container->getOffsetByToken($token);
+        $iterator->seek($key);
+
+        $level = 0;
+        $inside = false;
+
+        while ($iterator->valid()) {
+            $token = $iterator->current();
+            if ($this->evaluateConstraint('IsOpeningCurlyBrace', $token)) {
+                if (0 == $level) {
+                    $inside = true;
+                }
+                $level++;
+            }
+            if ($this->evaluateConstraint('IsClosingCurlyBrace', $token)) {
+                $level--;
+            }
+            $result->addToken($token);
+
+            if ($inside && 0 == $level) {
+                break;
+            }
+            $iterator->next();
+        }
+        return $result;
+    }
+}
---
+++ tests/Tests/PHP/Manipulator/TokenFinder/SwitchFinderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\PHP\Manipulator\TokenFinder;
+
+use PHP\Manipulator\TokenFinder\Result;
+use PHP\Manipulator\TokenFinder\SwitchFinder;
+use PHP\Manipulator\TokenContainer;
+use PHP\Manipulator\Token;
+
+/**
+ * @group TokenFinder\SwitchFinder
+ */
+class SwitchFinderTest
+extends \Tests\TestCase
+{
+
+    /**
+     * @return array
+     */
+    public function findProvider()
+    {
+        $data = array();
+        $path = '/TokenFinder/SwitchFinder/';
+
+        #0
+        $data[] = array(
+            $c = $this->getContainerFromFixture($path . 'input0'),
+            $c[2],
+            $this->getResultFromContainer($c, 2, 60),
+        );
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider findProvider
+     * @covers \PHP\Manipulator\TokenFinder\SwitchFinder::find
+     * @covers \PHP\Manipulator\TokenFinder\SwitchFinder::<protected>
+     */
+    public function testFind($container, $token, $expectedResult)
+    {
+        $finder = new SwitchFinder();
+        $actualResult = $finder->find($token, $container);
+        $this->assertFinderResultsMatch($expectedResult, $actualResult);
+    }
+}
