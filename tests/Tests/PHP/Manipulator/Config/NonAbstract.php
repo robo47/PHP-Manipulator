a92f1a72b465f087c71900150ccc2b339d478990
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-20 11:53:26 +0200

    Excluded Config\Nonabstract

---
+++ tests/Tests/PHP/Manipulator/Config/NonAbstract.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests\PHP\Manipulator\Config;
+
+use PHP\Manipulator\Config;
+
+class NonAbstract extends Config
+{
+
+    public $data;
+
+    protected function _initConfig($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setOption($option, $value)
+    {
+        $this->_options[$option] = $value;
+    }
+}
