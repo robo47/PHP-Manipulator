22124c782c8002dfb9dc7eb150919e9c0e982c8d
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-04-25 19:52:53 +0200

    fixed problem with unittests based on used php.ini
    On my server unittests failed because of redifining constants ...

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -5,16 +5,17 @@ use PHP\Manipulator\Autoloader;
 
 error_reporting(E_ALL | E_STRICT);
 
-define('BASE_PATH', realpath(dirname(__FILE__ ) . '/../'));
-define('TESTS_PATH', BASE_PATH . '/tests/');
+if(!defined('BASE_PATH')) {
+    define('BASE_PATH', realpath(dirname(__FILE__ ) . '/../'));
+    define('TESTS_PATH', BASE_PATH . '/tests/');
+    $paths = array();
+    $paths[] = BASE_PATH . '/library/';
+    $paths[] = TESTS_PATH;
+    $paths[] = get_include_path();
 
-$paths = array();
-$paths[] = BASE_PATH . '/library/';
-$paths[] = TESTS_PATH;
-$paths[] = get_include_path();
-
-// Include path
-set_include_path(implode($paths, PATH_SEPARATOR));
+    // Include path
+    set_include_path(implode($paths, PATH_SEPARATOR));
+}
 
 // Register autoloader
 require_once 'PHP/Manipulator/Autoloader.php';
