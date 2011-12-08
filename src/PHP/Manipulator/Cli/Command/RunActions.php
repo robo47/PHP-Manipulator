3ec96e307bfa6fe760291e28eaa4fd95996870e6
Benjamin Steininger <robo47@robo47.net>
2011-12-08 22:12:15 +0100

    Fix in PHP\Manipulator\Cli\Command\RunActions for changed autoloader

--- src/PHP/Manipulator/Cli/Command/RunActions.php
+++ src/PHP/Manipulator/Cli/Command/RunActions.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\HttpFoundation\UniversalClassLoader;
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 /**
  * @package PHP\Manipulator
