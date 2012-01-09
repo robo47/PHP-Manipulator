2ee61b2a820b458ec8b20a41e0135e0585efd555
Benjamin Steininger <robo47@robo47.net>
2012-01-09 07:39:37 +0100

    phpunit only on test-env

--- bin/ci/install_dependencies.sh
+++ bin/ci/install_dependencies.sh
@@ -1,19 +1,7 @@
 pyrus install pecl/xdebug
 
-pyrus channel-discover pear.phpmd.org
-pyrus channel-discover pear.pdepend.org
-pyrus install phpmd/PHP_PMD
-pyrus install pdepend/PHP_Depend-beta
-pyrus install pear/PHP_CodeSniffer
-
 pyrus channel-discover pear.phpunit.de
-pyrus channel-discover components.ez.no
-pyrus channel-discover pear.symfony-project.com
 
 pyrus install phpunit/PHPUnit
-
-pyrus install phpunit/phpcpd
-pyrus install phpunit/phploc
-pyrus install phpunit/PHP_CodeBrowser
 
 phpenv rehash
